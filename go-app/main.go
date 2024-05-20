422a6788cf7fc5bb43a70b4cfba6607dec948e1c
Fuzail <fuzail@xyz.com>
2024-05-20 21:59:53 +0530

    Add new files to go-app

---
+++ go-app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", helloHandler)
+    http.ListenAndServe(":8080", r)
+}
+
