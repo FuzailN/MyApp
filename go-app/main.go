34e7bc3c8c477e683c97bf13aeebb748deac795a
Fuzail <fuzail@xyz.com>
2024-05-21 14:12:57 +0530

    updated go-apps files adding

--- go-app/main.go
+++ go-app/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
     "github.com/gorilla/mux"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+var templates = template.Must(template.ParseFiles("templates/layout.html", "templates/home.html", "templates/about.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+    err := templates.ExecuteTemplate(w, tmpl, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "layout.html", map[string]interface{}{
+        "Title": "Home",
+        "ContentTemplate": "home.html",
+    })
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "layout.html", map[string]interface{}{
+        "Title": "About",
+        "ContentTemplate": "about.html",
+    })
 }
 
 func main() {
     r := mux.NewRouter()
-    r.HandleFunc("/", helloHandler)
+    r.HandleFunc("/", homeHandler)
+    r.HandleFunc("/about", aboutHandler)
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
     http.ListenAndServe(":8080", r)
 }
 
